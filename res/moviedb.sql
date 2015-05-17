12112739a5aabc2e7d9c86a4cae93e2c8d34a6cc
Sven <sven.willrich@googlemail.com>
2015-05-17 11:29:11 +0200

    redesign database schema

--- res/moviedb.sql
+++ res/moviedb.sql
@@ -22,10 +22,10 @@ CREATE TABLE moviedb.persontype (
 	CONSTRAINT pk_persontype PRIMARY KEY ( id )
  );
 
-CREATE TABLE moviedb.rate ( 
+CREATE TABLE moviedb.rating ( 
 	id                   integer  NOT NULL,
 	movie                integer  NOT NULL,
-	rate                 integer  NOT NULL,
+	rating               integer  NOT NULL,
 	CONSTRAINT pk_bewertung PRIMARY KEY ( id ),
 	CONSTRAINT pk_bewertung_0 UNIQUE ( movie ) 
  );
@@ -41,6 +41,16 @@ CREATE TABLE moviedb.person (
 
 CREATE INDEX idx_person ON moviedb.person ( persontype );
 
+CREATE TABLE moviedb.mivehasdirector ( 
+	director             integer  NOT NULL,
+	movie                integer  NOT NULL,
+	CONSTRAINT idx_mivehasdirector PRIMARY KEY ( director, movie )
+ );
+
+CREATE INDEX idx_mivehasdirector_0 ON moviedb.mivehasdirector ( movie );
+
+CREATE INDEX idx_mivehasdirector_1 ON moviedb.mivehasdirector ( director );
+
 CREATE TABLE moviedb.moviehasperson ( 
 	movie                integer  NOT NULL,
 	person               integer  NOT NULL,
@@ -51,6 +61,10 @@ CREATE INDEX idx_filmhatperson_0 ON moviedb.moviehasperson ( movie );
 
 CREATE INDEX idx_filmhatperson_1 ON moviedb.moviehasperson ( person );
 
+ALTER TABLE moviedb.mivehasdirector ADD CONSTRAINT fk_mivehasdirector FOREIGN KEY ( movie ) REFERENCES moviedb.movie( id );
+
+ALTER TABLE moviedb.mivehasdirector ADD CONSTRAINT fk_mivehasdirector_0 FOREIGN KEY ( director ) REFERENCES moviedb.person( id );
+
 ALTER TABLE moviedb.movie ADD CONSTRAINT fk_film_genre FOREIGN KEY ( genre ) REFERENCES moviedb.genre( id );
 
 ALTER TABLE moviedb.moviehasperson ADD CONSTRAINT fk_filmhatperson FOREIGN KEY ( movie ) REFERENCES moviedb.movie( id );
@@ -59,5 +73,5 @@ ALTER TABLE moviedb.moviehasperson ADD CONSTRAINT fk_filmhatperson_0 FOREIGN KEY
 
 ALTER TABLE moviedb.person ADD CONSTRAINT fk_person FOREIGN KEY ( persontype ) REFERENCES moviedb.persontype( id );
 
-ALTER TABLE moviedb.rate ADD CONSTRAINT fk_bewertung FOREIGN KEY ( movie ) REFERENCES moviedb.movie( id );
+ALTER TABLE moviedb.rating ADD CONSTRAINT fk_bewertung FOREIGN KEY ( movie ) REFERENCES moviedb.movie( id );
 
